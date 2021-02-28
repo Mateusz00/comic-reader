f9e5c2a28d86611a3cc1e1d70637adcd09bfc4ac
Mateusz Sęk <mateusz.sek.00@protonmail.com>
2021-02-28 17:08:45 +0100

    maven init

---
+++ src/main/java/io/github/mateusz00/ComicReader/ComicReaderApplication.java
@@ -0,0 +1,12 @@
+package io.github.mateusz00.ComicReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicReaderApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ComicReaderApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/mateusz00/ComicReader/ComicReaderApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.mateusz00.ComicReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicReaderApplicationTests
+{
+    @Test
+    void contextLoads() {
+    }
+}
